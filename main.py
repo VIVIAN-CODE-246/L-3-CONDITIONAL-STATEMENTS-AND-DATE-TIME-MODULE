317849043773126cd65837b62409f8f87edfd176
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2025-01-11 13:47:55 +0200

    PROJECT ON CONDITIONAL STATEMENT

--- main.py
+++ main.py
@@ -31,3 +31,20 @@ import datetime
 print(datetime.datetime.now())
 import calendar
 print(calendar.calendar(1))
+
+number = 10
+if number > 50:
+    print("Yes")
+elif number < 50:
+    print("No")
+else:
+    print("Invalid Input")
+
+num = float(input("Enter your age:"))
+if num>=0:
+    if num > 18:
+        print("Adult")
+    else:
+        print("Underage")
+else:
+    print("Wrond Input")
